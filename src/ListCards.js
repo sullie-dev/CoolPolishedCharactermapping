6303efdd63a3a8ff53be6ea45924f1cce861fdb4
sullie-dev <eoghan@sullie.dev>
2020-09-18 12:57:19 +0100

    Adjusting the loading animation to occur on page load and removed animation timer

--- src/ListCards.js
+++ src/ListCards.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./ListCards.css";
 
-import axios from 'axios'
-import Card from './Card';
+import axios from "axios";
+import Card from "./Card";
 
-class ListCards extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            cardInfo:[]
-        }
-    }
-    async componentDidMount(){
-      const cardID = this.props.match.params.id
-      const url = `https://waterlogged-bland-startup.herokuapp.com/${cardID}`;
-      const nocors = `https://cors-anywhere.herokuapp.com/`;
-        axios.get( nocors + url)
-        .then(response => {
-            this.setState({cardInfo:[...this.state.cardInfo, response.data]})
-        }).catch(err => console.error(err));
-    }
-    render(){
-        const listC = this.state.cardInfo.map((c) => {
-            return (
-              <div>
-                <Card cardInfo={c} key={uuidv4()} />
-              </div>
-            );
-          });
-      
-          return (
-            <div>
-              <h1>CardViewer</h1>
-              {listC}
-            </div>
-          );
-    }
+class ListCards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardInfo: [],
+      is_loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const cardID = this.props.match.params.id;
+    const url = `https://waterlogged-bland-startup.herokuapp.com/${cardID}`;
+    const nocors = `https://cors-anywhere.herokuapp.com/`;
+    axios
+      .get(nocors + url)
+      .then((response) => {
+        this.setState({
+          cardInfo: [...this.state.cardInfo, response.data],
+          is_loading: false,
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+  render() {
+    const listC = this.state.cardInfo.map((c) => {
+      return <Card cardInfo={c} key={uuidv4()} />;
+    });
+
+    return (
+      <div className={!this.state.is_loading ? "" : "triforce-container"}>
+        <h1>CardViewer</h1>
+        <div className={!this.state.is_loading ? "" : "triforce"}></div>
+        {listC}
+      </div>
+    );
+  }
 }
 
-export default ListCards
+export default ListCards;
